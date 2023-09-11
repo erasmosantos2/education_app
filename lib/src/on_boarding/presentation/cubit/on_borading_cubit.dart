589008d5f4cb200a0933be64e2239a315240a7ee
eromweb <eromweb@gmail.com>
2023-09-10 21:25:54 -0300

    presentation

---
+++ lib/src/on_boarding/presentation/cubit/on_borading_cubit.dart
@@ -0,0 +1,36 @@
+import 'package:bloc/bloc.dart';
+import 'package:education_app/src/on_boarding/domain/usecases/cache_first_timer.dart';
+import 'package:education_app/src/on_boarding/domain/usecases/check_if_user_is_first_timer.dart';
+import 'package:education_app/src/on_boarding/presentation/cubit/on_borading_state.dart';
+
+class OnboardingCubit extends Cubit<OnBoardingState> {
+  OnboardingCubit({
+    required CacheFirstTimer cacheFirstTimer,
+    required CheckIfUserIsFirstTimer checkIfUserIsFirstTimer,
+  })  : _cacheFirstTimer = cacheFirstTimer,
+        _checkIfUserIsFirstTimer = checkIfUserIsFirstTimer,
+        super(const OnBoardingInitial());
+
+  final CacheFirstTimer _cacheFirstTimer;
+  final CheckIfUserIsFirstTimer _checkIfUserIsFirstTimer;
+
+  Future<void> cacheFirstTimer() async {
+    emit(CachingFirstTimer());
+    final result = await _cacheFirstTimer();
+
+    result.fold(
+      (failure) => emit(OnBoardingError(failure.errorMessage)),
+      (r) => emit(const UserCached()),
+    );
+  }
+
+  Future<void> checkIfUserIsFirstTimer() async {
+    emit(const CheckingIfUserIsFirstTimer());
+    final result = await _checkIfUserIsFirstTimer();
+
+    result.fold(
+      (failure) => emit(const OnBoardingStatus(isFirstTimer: true)),
+      (status) => emit(OnBoardingStatus(isFirstTimer: status)),
+    );
+  }
+}
