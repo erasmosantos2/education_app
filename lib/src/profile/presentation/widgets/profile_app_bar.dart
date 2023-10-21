d04a9975443b438d739047d36148e49aeb985197
eromweb <eromweb@gmail.com>
2023-10-21 20:17:09 -0300

    profile avatar presentation

--- lib/src/profile/presentation/widgets/profile_app_bar.dart
+++ lib/src/profile/presentation/widgets/profile_app_bar.dart
@@ -2,9 +2,13 @@ import 'dart:async';
 
 import 'package:education_app/core/extensions/context_extension.dart';
 import 'package:education_app/core/res/colours.dart';
+import 'package:education_app/core/services/injection_container.dart';
+import 'package:education_app/src/auth/presentation/bloc/auth_bloc.dart';
+import 'package:education_app/src/profile/presentation/views/edit_profile_view.dart';
 import 'package:education_app/src/profile/presentation/widgets/popup_item.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:iconly/iconly.dart';
 
 class ProfileAppBar extends StatelessWidget implements PreferredSizeWidget {
@@ -30,12 +34,17 @@ class ProfileAppBar extends StatelessWidget implements PreferredSizeWidget {
           ),
           itemBuilder: (_) => [
             PopupMenuItem<void>(
-              onTap: () => context.push(const Placeholder()),
               child: const PopupItem(
                 title: 'Edit Profile',
                 icon: Icon(
                   Icons.edit_outlined,
                   color: Colours.neutralTextColour,
+                ),
+              ),
+              onTap: () => context.push(
+                BlocProvider(
+                  create: (_) => sl<AuthBloc>(),
+                  child: const EditProfileView(),
                 ),
               ),
             ),
